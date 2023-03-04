68a6c2aba1ef068d15344696215a44f07a94bb95
ali8810am <ali8810.am@gmail.com>
2023-03-04 19:27:27 +0330

    Add project files.

---
+++ HotelListing/Program.cs
@@ -0,0 +1,54 @@
+using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File(
+        path: "C:\\Users\\SamRayane\\Documents\\Visual Studio 2022\\visual stadio\\Logs\\asp.net core\\hotelListing\\log-.txt",
+        outputTemplate: "{TimeStamp: yyyy-MM-dd HH:mm:ss.fff zzz}[{Level:u3}] {Message:lg}{NewLine}{Exception}",
+        rollingInterval: RollingInterval.Day,
+        restrictedToMinimumLevel: LogEventLevel.Information
+    ).CreateLogger();
+try
+{
+    Log.Information("Application Is Starting");
+
+
+
+    var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+    builder.Host.UseSerilog();
+    builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+
+    var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+
+
+catch (Exception e)
+{
+    Log.Fatal(e, "Application Failed to start");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
